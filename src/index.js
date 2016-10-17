5b2217f0324ac5992c16f33a8f869a879721561a
Raymond Hammarling <raykay.rh@gmail.com>
2016-10-17 03:28:20 +0100

    Implement brief IRC

--- src/index.js
+++ src/index.js
@@ -1,8 +1,74 @@
 import deepstream from 'deepstream.io-client-js';
+import IrcClient from 'squelch-client';
+import loadJsonFile from 'load-json-file';
+import R from 'ramda';
+import Rx from 'rxjs/Rx';
 
-const client = deepstream('localhost:6021').login({});
-const r = client.record.getRecord('horp/dorp');
+const setPath = R.curry((path, value, record) => record.set(path, value));
 
-setInterval(() => {
-    r.set({ woah: Math.floor(Math.random() * 50) });
-}, 1000);
+function whenReady(record) {
+    return new Promise(resolve => {
+        record.whenReady(resolve);
+    });
+}
+
+function eventObservable(event, dsClient) {
+    return Rx.Observable.fromEventPattern(
+        (h) => dsClient.event.subscribe(event, h),
+        (h) => dsClient.event.unsubscribe(event, h)
+    );
+}
+
+async function manageClientChannel(config, dsClient, ircClient, channel) {
+    console.log('Managing', 'channel/' + config.server + '/' + channel);
+
+    const channelRecord = dsClient.record.getRecord('channel/' + config.server + '/' + channel);
+
+    await whenReady(channelRecord);
+
+    channelRecord.set({ messages: [] });
+
+    Rx.Observable.fromEvent(ircClient, 'msg')
+        .filter(R.propEq('to', channel))
+        .map(o => ({ ...o, id: dsClient.getUid() }))
+        .bufferCount(10, 1)
+        .debounceTime(20)
+        .subscribe(setPath('messages', R.__, channelRecord));
+
+    eventObservable('send-message', dsClient)
+        .filter(R.allPass([
+            R.propEq('server', config.server),
+            R.propEq('channel', channel)
+        ]))
+        .subscribe(R.pipe(
+            R.prop('message'),
+            R.curryN(2, ::ircClient.msg)(channel)
+        ));
+}
+
+async function manageClient(config, dsClient) {
+    const ircClient = new IrcClient({
+        ...config,
+        autoConnect: false, triggerEventsForOwnMessages: true
+    });
+
+    await ircClient.connect();
+
+    await Promise.all(
+        config.channels.map(
+            manageClientChannel.bind(null, config, dsClient, ircClient)
+        )
+    );
+}
+
+async function main() {
+    const dsClient = deepstream('localhost:6021').login({});
+
+    const config = await loadJsonFile('./irc-config.json');
+
+    await Promise.all(
+        config.map(c => manageClient(c, dsClient))
+    );
+}
+
+main().catch(e => console.error(e.stack));
--- src/web/index.js
+++ src/web/index.js
@@ -2,12 +2,28 @@
 import deepstream from 'deepstream.io-client-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import stringHash from 'string-hash';
+
+import qs from 'querystring';
 
 import styles from './style/index.css';
 
-const client = deepstream(location.hostname + ':6020').login({});
+const query = qs.parse(location.search.slice(1));
+const client = deepstream(location.host).login({});
 
-class HorpDorp extends React.Component {
+class Message extends React.Component {
+    render() {
+        const { msg } = this.props;
+        const nickColor = styles['nick-color-' + (stringHash(msg.from) % 8 + 1)];
+
+        return <li className={ styles['message-entry'] }>
+            <div className={ styles['message-from'] + ' ' + nickColor }>{ msg.from }</div>
+            <div className={ styles['message-msg'] }>{ msg.msg }</div>
+        </li>;
+    }
+}
+
+class App extends React.Component {
     constructor(props) {
         super(props);
 
@@ -22,14 +38,30 @@ class HorpDorp extends React.Component {
         this.props.record.unsubscribe();
     }
 
-    render() {
-        const style = this.state.woah > 25 ? styles.banana : styles.apple;
+    onKeyPress(event) {
+        if(event.key === 'Enter') {
+            client.event.emit('send-message', { ...query, message: event.target.value });
+            event.target.value = '';
+        }
+    }
 
-        return <div className={style}>{ this.state.woah }</div>;
+    render() {
+        return <div className={ styles.main }><div className={ styles['inner-wrap'] }>
+            <div className={ styles.messages }>
+                <ul className={ styles['messages-list'] }>{
+                    (this.state.messages || []).map(msg => <Message key={ msg.id } msg={msg} />)
+                }</ul>
+            </div>
+            <div className={ styles['message-entry'] }>
+                <input className={ styles.input } type="text" onKeyPress={this.onKeyPress}/>
+            </div>
+        </div></div>;
     }
 }
 
+const record = client.record.getRecord(`channel/${query.server}/${query.channel}`);
+
 ReactDOM.render(
-    <HorpDorp record={client.record.getRecord('horp/dorp')} />,
+    <App record={record} />,
     document.getElementById('container')
 );
